92e61fa488cc73d8575a06ee2669d035f598eae3
Maksymilian <mblazejczyk1998@gmail.com>
2022-11-22 00:55:44 +0100

    First working aplication with use of JavaScript

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 let count = 0;
 let countElement = document.getElementById("count-el");
+let saves = " | ";
+let savesElement = document.getElementById("saves");
 
 function increment() {
     count += 1;
     countElement.innerText = count;
-    console.log(count);
 }
 
 function reset() {
@@ -13,5 +14,12 @@ function reset() {
 }
 
 function save() {
-    
+    saves = saves + count + " | ";
+    savesElement.innerText = saves;
+}
+
+function resetAll() {
+    saves = "";
+    savesElement.innerText = "None";
+    saves = " | ";
 }
